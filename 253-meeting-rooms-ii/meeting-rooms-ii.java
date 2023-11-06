3a1518dd86f8407a8c47e68ed9305a1cac61ceb7
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2023-11-05 21:46:44 -0500

    Time: 8 ms (70.18%) | Memory: 44.2 MB (71.38%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -7,12 +7,10 @@ class Solution {
         });
         // for(int i=0; i<intervals.length; i++) System.out.println(intervals[i][1]);
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int minMeetingRooms = 0;
         for (int i=0; i<intervals.length; i++) {
-            while (!pq.isEmpty() && intervals[i][0] >= pq.peek()) pq.poll();
+            if (!pq.isEmpty() && intervals[i][0] >= pq.peek()) pq.poll();
             pq.add(intervals[i][1]);
-            minMeetingRooms = Math.max(minMeetingRooms, pq.size());
         }
-        return minMeetingRooms;
+        return pq.size();
     }
 }
