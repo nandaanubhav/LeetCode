f5459d4846bb25745c8743c0a2e6df81990e29d5
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2023-11-10 14:29:30 -0500

    Time: 5 ms (33.84%) | Memory: 46.2 MB (19.48%) - LeetSync

--- 1816-lowest-common-ancestor-of-a-binary-tree-iv/lowest-common-ancestor-of-a-binary-tree-iv.java
+++ 1816-lowest-common-ancestor-of-a-binary-tree-iv/lowest-common-ancestor-of-a-binary-tree-iv.java
@@ -9,23 +9,21 @@
  */
 class Solution {
     TreeNode ans;
+    HashSet<Integer> nodeList;
     private int findLCA(TreeNode node, TreeNode[] nodes) {
         if(node == null) return 0;
         int mid = 0;
         int left = findLCA(node.left, nodes);
         int right = findLCA(node.right, nodes);
-        for(int i=0; i<nodes.length; i++) {
-            if(nodes[i] == node) {
-                mid++;
-                break;
-            }
-        }
+        if(nodeList.contains(node.val)) mid = 1;
         int sum = left + right + mid;
         if(sum == nodes.length && ans == null) ans = node;
         return left + right + mid;
 
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        nodeList = new HashSet<>();
+        for(int i=0; i<nodes.length; i++) nodeList.add(nodes[i].val);
         findLCA(root, nodes);
         return ans;
     }
