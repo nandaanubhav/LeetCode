5a87adf102495738f997f924bd3c724767c98343
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2023-12-22 23:54:45 -0500

    Time: 10 ms (7.49%) | Memory: 42.2 MB (5.51%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        Set<String> visited = new HashSet<>();
+        int currX = 0, currY = 0;
+        visited.add(currX + "-" + currY);
+        for(int i=0; i<path.length(); i++) {
+            char c = path.charAt(i);
+            if(c == 'N') currY++;
+            else if (c == 'E') currX++;
+            else if (c == 'S') currY--;
+            else if (c == 'W') currX--;
+            String currPos = currX + "-" + currY;
+            System.out.println(currPos);
+            if (visited.contains(currPos)) return true;
+            else visited.add(currPos);
+        }
+        return false;
+    }
+}
