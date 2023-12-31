0d4901ec2d21518dbf2a8eeae684a943fe1938ce
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2023-12-30 19:26:54 -0500

    Time: 4 ms (32.33%) | Memory: 41.2 MB (18.55%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -1,16 +1,16 @@
 
 import java.util.Hashtable;class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
-        Map<Character, Integer> endPosition = new HashMap<>();
+        Map<Character, Integer> startPosition = new HashMap<>();
         int n = s.length();
         int maxLength = -1;
         for(int i=0; i<n; i++) {
             char c = s.charAt(i);
-            int prevPosition = endPosition.getOrDefault(c, -1);
+            int prevPosition = startPosition.getOrDefault(c, -1);
             if(prevPosition >= 0) {
                 maxLength = Math.max(maxLength, i - prevPosition - 1);
             }
-            endPosition.putIfAbsent(s.charAt(i), i);
+            startPosition.putIfAbsent(s.charAt(i), i);
         }
         return maxLength;
     }
