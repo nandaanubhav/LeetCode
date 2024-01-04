908e47db8b1827899b7f20b9d74c3529e6ca4258
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-01-03 20:51:37 -0500

    Time: 19 ms (73.58%) | Memory: 62.9 MB (12.45%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
@@ -8,7 +8,7 @@ class Solution {
         for(Map.Entry<Integer, Integer> entry: freqCounter.entrySet()) {
             int val = entry.getValue();
             if(val == 1) return -1;
-            operations += (int) (val / 3) + (val % 3 == 0 ? 0 : 1 );
+            operations += Math.ceilDiv(val, 3);
         }
         return operations;
     }
