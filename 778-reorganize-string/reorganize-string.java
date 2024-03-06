ec67f60a7f5bea712388a42a1d5ee1a814a9489e
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-05 20:36:20 -0500

    Time: 1 ms (93.82%) | Memory: 41.7 MB (49.50%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String reorganizeString(String s) {
+        int[] charSet = new int[26];
+        int n = s.length();
+        for(char c : s.toCharArray()) charSet[c - 'a']++;
+        int maxFreq = 0, letter = 0;
+        for(int i=0; i<26; i++) {
+            if(charSet[i] > maxFreq) {
+                maxFreq = charSet[i];
+                letter = i;
+            }
+        }
+        if(maxFreq > Math.ceil(n/2.0)) return "";
+        char[] ans = new char[n];
+        int index = 0;
+        while(charSet[letter] > 0) {
+            ans[index] = (char) (letter + 'a');
+            index += 2;
+            charSet[letter]--;
+        }
+        for (int i = 0; i < charSet.length; i++) {
+            while (charSet[i] > 0) {
+                if (index >= s.length()) {
+                    index = 1;
+                }
+                ans[index] = (char) (i + 'a');
+                index += 2;
+                charSet[i]--;
+            }
+        }
+        return String.valueOf(ans);
+    }
+}
