77e8313f43cb2891171b8835d01b109ad27ae7ba
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-06 21:20:33 -0500

    Time: 2 ms (94.05%) | Memory: 56.8 MB (79.67%) - LeetSync

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.java
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.java
@@ -2,19 +2,23 @@ class Solution {
     public int minimumSwaps(int[] nums) {
         int n = nums.length;
         if(n == 1) return 0;
-        int maxElement = nums[0], maxElementPos = 0, minElement = nums[n-1] , minElementPos = n-1;
+        int maxElement = nums[0], maxElementPos = 0, minElement = nums[0] , minElementPos = 0;
         for(int i=0; i<n; i++) {
             if(nums[i] >= maxElement) {
                 maxElement = nums[i];
                 maxElementPos = i;
             }
-        }
-        for(int i=n-1; i>=0; i--) {
-            if(nums[i] <= minElement) {
+            if(nums[i] < minElement) {
                 minElement = nums[i];
                 minElementPos = i;
             }
         }
+        // for(int i=n-1; i>=0; i--) {
+        //     if(nums[i] <= minElement) {
+        //         minElement = nums[i];
+        //         minElementPos = i;
+        //     }
+        // }
         // System.out.println("maxElementPos " + maxElementPos);
         // System.out.println("minElementPos " + minElementPos);
         int maxElementSwaps = n - maxElementPos - 1;
