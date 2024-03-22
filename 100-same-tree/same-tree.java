8a0148aadd027fb4891af8f52f6a9de744fd1432
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-22 15:59:33 -0400

    Time: 0 ms (100.00%) | Memory: 40.9 MB (60.40%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,10 +14,12 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    private boolean dfsUtil(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
-        if(p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return p.val == q.val && dfsUtil(p.left, q.left) && dfsUtil(p.right, q.right);
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return dfsUtil(p, q);
     }
 }
