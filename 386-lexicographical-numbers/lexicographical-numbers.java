de16fbadb0ef54782924233389489adc6c9d2444
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-24 16:22:30 -0400

    Time: 21 ms (13.02%) | Memory: 48.2 MB (72.09%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,12 +1,26 @@
 class Solution {
-    public List<Integer> lexicalOrder(int n) {
-        List<String> numbers = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        for(int i=1; i<=n; i++) {
-            numbers.add(String.valueOf(i));
+    Comparator<Integer> comp = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer i1, Integer i2) {
+            String s1 = String.valueOf(i1), s2 = String.valueOf(i2);
+            int i = 0;
+            while(i < s1.length() && i < s2.length()) {
+                if(s1.charAt(i) == s2.charAt(i)) {
+                    i++;
+                } else {
+                    return (int) (Integer.valueOf(s1.charAt(i)) - Integer.valueOf(s2.charAt(i)));
+                }
+            }
+            return (int) (s1.length() - s2.length());
         }
-        Collections.sort(numbers);
-        numbers.stream().mapToInt(i -> Integer.valueOf(i)).forEach(i -> ans.add(i));
-        return ans;
+    };
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=1; i<=n; i++) {
+            numbers.add(i);
+        }
+        Collections.sort(numbers, comp);
+        return numbers;
     }
 }
