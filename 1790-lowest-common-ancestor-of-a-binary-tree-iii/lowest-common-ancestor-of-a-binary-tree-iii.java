8e525079213d55bd5f679d02d4d6752b8edef21d
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-26 01:28:06 -0400

    Time: 30 ms (41.81%) | Memory: 44.7 MB (36.92%) - LeetSync

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -0,0 +1,26 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Set<Node> seen = new HashSet<>();
+        while(p != null) {
+            seen.add(p);
+            p = p.parent;
+        }
+        while(q!=null) {
+            if(seen.contains(q)) {
+                return q;
+            }
+            q = q.parent;
+        }
+        return null;
+    }
+}
