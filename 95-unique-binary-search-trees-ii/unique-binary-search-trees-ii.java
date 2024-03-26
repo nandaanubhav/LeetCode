0fa5d9e8f09668ef709e7271805a7146d533e999
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-26 11:08:09 -0400

    Time: 1 ms (95.32%) | Memory: 44.2 MB (92.01%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.java
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.java
@@ -1,58 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        List<List<List<TreeNode>>> dp = new ArrayList<>(n + 1);
+        List<List<TreeNode>> dp = new ArrayList<>(n + 1);
         for (int i = 0; i <= n; i++) {
-            List<List<TreeNode>> innerList = new ArrayList<>(n + 1);
-            for (int j = 0; j <= n; j++) {
-                innerList.add(new ArrayList<>());
-            }
-            dp.add(innerList);
+            dp.add(new ArrayList<>());
         }
-        for (int i = 1; i <= n; i++) {
-            dp.get(i).get(i).add(new TreeNode(i));
-        }
-         for (int numberOfNodes = 2; numberOfNodes <= n; numberOfNodes++) {
-            for (int start = 1; start <= n - numberOfNodes + 1; start++) {
-                int end = start + numberOfNodes - 1;
-                for (int i = start; i <= end; i++) {
-                    List<TreeNode> leftSubtrees =
-                        (i != start) ? dp.get(start).get(i - 1) : new ArrayList<>();
-                    if (leftSubtrees.isEmpty()) {
-                        leftSubtrees.add(null);
-                    }
-                    List<TreeNode> rightSubtrees =
-                        (i != end) ? dp.get(i + 1).get(end) : new ArrayList<>();
-                    if (rightSubtrees.isEmpty()) {
-                        rightSubtrees.add(null);
-                    }
+        dp.get(0).add(null);
 
-                    for (TreeNode left : leftSubtrees) {
-                        for (TreeNode right : rightSubtrees) {
-                            TreeNode root = new TreeNode(i, left, right);
-                            dp.get(start).get(end).add(root);
-                        }
+        for (int numberOfNodes = 1; numberOfNodes <= n; numberOfNodes++) {
+            for (int i = 1; i <= numberOfNodes; i++) {
+                int j = numberOfNodes - i;
+                for (TreeNode left : dp.get(i - 1)) {
+                    for (TreeNode right : dp.get(j)) {
+                        TreeNode root = new TreeNode(i, left, clone(right, i));
+                        dp.get(numberOfNodes).add(root);
                     }
                 }
             }
         }
-        return dp.get(1).get(n);
-        
+        return dp.get(n);
+    }
 
-
+    private TreeNode clone(TreeNode node, int offset) {
+        if (node == null) {
+            return null;
+        }
+        TreeNode clonedNode = new TreeNode(node.val + offset);
+        clonedNode.left = clone(node.left, offset);
+        clonedNode.right = clone(node.right, offset);
+        return clonedNode;
     }
 }
