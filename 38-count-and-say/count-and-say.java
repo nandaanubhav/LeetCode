03b8ce2cdb42b7780a204a0b3a8928b94a0f9578
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-26 22:55:06 -0400

    Time: 8 ms (38.59%) | Memory: 41.2 MB (67.52%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -9,13 +9,11 @@ class Solution {
             for(int j=1; j<prev.length(); j++) {
                 if(prev.charAt(j) == prev.charAt(j-1)) count++;
                 else {
-                    sb.append(count);
-                    sb.append(prev.charAt(j-1));
+                    sb.append(count).append(prev.charAt(j-1));
                     count = 1;
                 }
             }
-            sb.append(count);
-            sb.append(prev.charAt(prev.length() - 1));
+            sb.append(count).append(prev.charAt(prev.length() - 1));
             dp[i] = sb.toString();
         }
         return dp[n-1];
