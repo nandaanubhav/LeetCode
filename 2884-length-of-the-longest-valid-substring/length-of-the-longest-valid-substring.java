759fbe90edecbb8b2e905028b8310a63174b8c55
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-29 15:41:33 -0400

    Time: 141 ms (66.16%) | Memory: 61.7 MB (86.51%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
@@ -1,18 +1,20 @@
 class Solution {
     public int longestValidSubstring(String word, List<String> forbidden) {
-        HashSet<String> set = new HashSet<>();
-        for(String s: forbidden) set.add(s);
-
+        Set<String> set = new HashSet<>(forbidden);
         int n = word.length();
         int l = 0, r = 0, ans = 0;
         while(r < n) {
             for(int i=r; i>r-10 && i>=l; i--) {
-                if(set.contains(word.substring(i, r+1))) {
+                String subString = word.substring(i, r+1);
+                // System.out.println(subString);
+                if(set.contains(subString)) {
                     l = i+1;
+                    // System.out.println("Match found moving l to " + l);
                     break;
                 }
             }
-            ans = Math.max(ans, r - l +1);
+            // System.out.println("Checking for length: " + r + " " + l + " + 1");
+            ans = Math.max(ans, r - l + 1);
             r++;
         }
         return ans;
