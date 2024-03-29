8cd524731d1a776579ed09a07f3a24eff3b392e0
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-29 17:25:11 -0400

    Time: 56 ms (97.08%) | Memory: 167.5 MB (10.74%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
@@ -1,18 +1,69 @@
+class TrieNode {
+    private final int R = 26;
+    private TrieNode[] links;
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+        isEnd = false;
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+
+    public void setEnd(boolean isEnd) {
+        this.isEnd = isEnd;
+    }
+}
 class Solution {
-    public int longestValidSubstring(String word, List<String> forbidden) {
-        Set<String> forbiddenWords = new HashSet<>(forbidden);
-        int left = 0, right = 0, n = word.length(), longestValidSubStrLen = 0;
-        while(right < n) {
-            for(int i=right; i>=right - 10 && i>=left; i--) {
-                String subString = word.substring(i, right + 1);
-                if(forbiddenWords.contains(subString)) {
-                    left = i + 1;
-                    break;
-                }
-            }
-            longestValidSubStrLen = Math.max(longestValidSubStrLen, right - left + 1);
-            right++;
+    TrieNode root;
+
+    private void save(String s) {
+        int r = s.length() - 1;
+        TrieNode node = root;
+        while(r >= 0) {
+            char ch = s.charAt(r);
+            if(!node.containsKey(ch)) node.put(ch, new TrieNode());
+            node = node.get(ch);
+            r--;
         }
-        return longestValidSubStrLen;
+        node.setEnd(true);;
+    }
+
+    private int findLeft(String word, int left, int right) {
+        int index = right;
+        TrieNode node = root;
+        while(index >= left) {
+            char ch = word.charAt(index);
+            if(!node.containsKey(ch)) return left;
+            node = node.get(ch);
+            if(node.isEnd()) return index + 1;
+            index--;
+        }
+        return left;
+    }
+
+    public int longestValidSubstring(String word, List<String> forbidden) {
+        root = new TrieNode();
+        for(String s: forbidden) save(s);
+        int l = 0, maxLength = 0;
+        for(int r = 0; r < word.length(); r++) {
+            l = findLeft(word, l, r);
+            maxLength = Math.max(maxLength, r - l + 1);
+        }
+        return maxLength;
     }
 }
