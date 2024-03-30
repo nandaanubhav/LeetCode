757f1c680a08ca7bedebdbc34bb00f476089ce0c
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-03-30 15:15:44 -0400

    Time: 9 ms (28.53%) | Memory: 44.1 MB (53.00%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -2,11 +2,12 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
         boolean[] dp = new boolean[n + 1];
+        HashSet<String> wordDictSet = new HashSet<>(wordDict);
         dp[0] = true;
         for(int i=1; i<=n; i++) {
             for(int j=0; !dp[i] && j < i; j++) {
-                System.out.println("checking for " + s.substring(j, i));
-                dp[i] = dp[j] && wordDict.contains(s.substring(j, i));
+                // System.out.println("checking for " + s.substring(j, i));
+                dp[i] = dp[j] && wordDictSet.contains(s.substring(j, i));
             }
         }
         return dp[n];
