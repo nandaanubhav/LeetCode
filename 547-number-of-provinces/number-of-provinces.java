fc5ec47acc86faea88c888f060bb9e3a9c2b8fae
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-04-02 12:40:01 -0400

    Time: 1 ms (89.61%) | Memory: 48.1 MB (20.14%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,24 +1,47 @@
-class Solution {
-    private void dfs(int[][] isConnected, int node, boolean[] seen) {
-        //out of bounds condition
-        seen[node] = true;
-        // int[] nodeConnectedList = isConnected[node];
-        for(int i=0; i<isConnected.length; i++) {
-            if(isConnected[node][i] == 1 && !seen[i]) {
-                dfs(isConnected, i, seen);
-            }
+class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(int size) {
+        parent = new int[size];
+        for(int i=0; i<size; i++) parent[i] = i;
+        rank = new int[size];
+    }
+
+    public int find(int x) {
+        if(x != parent[x]) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int xSet = find(x);
+        int ySet = find(y);
+
+        if(xSet == ySet) return;
+        else if (rank[xSet] > rank[ySet]) {
+            parent[ySet] = xSet;
+        } else if (rank[ySet] > rank[xSet]) {
+            parent[xSet] = ySet;
+        } else {
+            parent[ySet] = xSet;
+            rank[xSet]++;
         }
     }
+}
+
+class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int noProvinces = 0;
-        boolean[] seen = new boolean[n];
+        UnionFind dsu = new UnionFind(n);
+        int noOfComponents = n;
         for(int i=0; i<n; i++) {
-            if(!seen[i]) {
-                noProvinces++;
-                dfs(isConnected, i, seen);
+            for(int j=i+1; j<n; j++) {
+                if(isConnected[i][j] == 1 && dsu.find(i) != dsu.find(j)) {
+                    noOfComponents--;
+                    dsu.union(i, j);
+                }
             }
         }
-        return noProvinces;
+        return noOfComponents;
     }
 }
