c2c7f1dac0aadea94ebffb0ea3aef5371b55dc14
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-04-03 19:46:32 -0400

    Time: 9 ms (28.60%) | Memory: 44.6 MB (20.34%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -4,10 +4,10 @@ class Solution {
         boolean[] dp = new boolean[n + 1];
         HashSet<String> wordDictSet = new HashSet<>(wordDict);
         dp[0] = true;
-        for(int i=1; i<=n; i++) {
-            for(int j=0; !dp[i] && j < i; j++) {
+        for(int right=1; right<=n; right++) {
+            for(int left=0; !dp[right] && left < right; left++) {
                 // System.out.println("checking for " + s.substring(j, i));
-                dp[i] = dp[j] && wordDictSet.contains(s.substring(j, i));
+                dp[right] = dp[left] && wordDictSet.contains(s.substring(left, right));
             }
         }
         return dp[n];
