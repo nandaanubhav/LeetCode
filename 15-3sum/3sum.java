81233f3711fade5bc748304db55747ac44e5abc2
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-04-07 17:54:31 -0400

    Time: 27 ms (90.54%) | Memory: 51.3 MB (76.61%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,16 +1,15 @@
 class Solution {
     List<List<Integer>> ans;
     private void twoSum(int[] nums, int index) {
-        HashSet<Integer> values = new HashSet<>();
-        for(int i=index+1; i<nums.length; i++) {
-            int complement = -nums[index] - nums[i];
-            if(values.contains(complement)) {
-                ans.add(Arrays.asList(nums[index], nums[i], complement));
-                while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-                    ++i;
-                }
-            }
-            values.add(nums[i]);
+        int left = index + 1, right = nums.length - 1;
+        while(left < right) {
+            int sum = nums[index] + nums[left] + nums[right];
+            if(sum == 0) {
+                ans.add(Arrays.asList(nums[index], nums[left++], nums[right--]));
+                while(left < right && nums[left] == nums[left - 1]) ++left;
+            } else if (sum > 0) {
+                right--;
+            } else if(sum < 0) left++;
         }
     }
     public List<List<Integer>> threeSum(int[] nums) {
