d069596f11c33ea1ff1ee5e6adc571d896a5b33a
Anubhav Nanda <20391731+nandaanubhav@users.noreply.github.com>
2024-04-07 17:41:14 -0400

    Time: 298 ms (32.51%) | Memory: 51.4 MB (60.70%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,26 @@
 class Solution {
-    List<List<Integer>> result;
-    public void findCompliment(int[] nums, int index) {
-        int l = index + 1, r = nums.length - 1;
-        while(l < r) {
-            int sum = nums[index] + nums[l] + nums[r];
-            if(sum == 0) {
-                result.add(Arrays.asList(nums[index], nums[l++], nums[r--]));
-                while(l < r && nums[l] == nums[l - 1]) ++l;
-            } else if (sum > 0) {
-                r--;
-            } else {
-                l++;
+    List<List<Integer>> ans;
+    private void twoSum(int[] nums, int index) {
+        HashSet<Integer> values = new HashSet<>();
+        for(int i=index+1; i<nums.length; i++) {
+            int complement = -nums[index] - nums[i];
+            if(values.contains(complement)) {
+                ans.add(Arrays.asList(nums[index], nums[i], complement));
+                while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                    ++i;
+                }
             }
+            values.add(nums[i]);
         }
     }
-
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        result = new ArrayList<>();
-        for(int i=0; i<nums.length && nums[i] <= 0; i++) {
-            if (i == 0 || nums[i] != nums[i-1])
-                findCompliment(nums, i);
+        ans = new ArrayList<>();
+        for(int i = 0; i<nums.length && nums[i] <= 0; i++) {
+            if(i == 0 || nums[i] != nums[i - 1]) {
+                twoSum(nums, i);
+            }
         }
-        return result;
+        return ans;
     }
 }
